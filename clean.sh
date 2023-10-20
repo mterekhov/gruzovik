7503439ec5c7972b0981ffa8b31db6d238ed4fb9
Michael <m.terekhov@icloud.com>
2023-10-20 17:04:30 +0300

    initial commit

---
+++ clean.sh
@@ -0,0 +1,12 @@
+rm -Rf build
+rm -Rf CMakeFiles
+rm -Rf CMakeScripts
+rm -Rf Debug
+rm -Rf DerivedData
+rm -Rf EagerLinkingTBDs
+rm -Rf EagerLinkingTBDs
+rm -Rf yajnavalkya.build
+rm cmake_install.cmake
+rm CMakeCache.txt
+rm -Rf yajnavalkya.xcodeproj
+rm -Rf Makefile
---
+++ gruzovik.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("This is GRUZOVIK\n");
+	return 0;
+}
